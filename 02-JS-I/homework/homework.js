0f31ace82d2545bd5b39c38f19438a7d17df8cea
gabicasas01 <casas.gf7@gmail.com>
2022-03-30 08:45:51 -0300

    First womework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,10 +4,10 @@
 const nuevaString = 'Hola a todos';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 07;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = 2>3;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
