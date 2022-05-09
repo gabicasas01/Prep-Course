b1f86bc9c2cda7cb34116ada2cf3db3b46691cf4
gabicasas01 <casas.gf7@gmail.com>
2022-05-09 15:59:07 -0300

    prueba de las 16hs

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -51,6 +51,9 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift (elemento);
+
+  return array;
 }
 
 
@@ -60,13 +63,21 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+    return palabras.join(' ');
 }
+
 
 
 function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  for(var i = 0; i < array.length; i++) {
+    if (array[i] === elemento) {
+      return true;
+    }
+  }
+  return false;
 }
 
 
