c3b6ea9e275ab1e43934c616f470b2afb0467df1
gabicasas01 <casas.gf7@gmail.com>
2022-05-09 16:33:59 -0300

    pruebas 16:30hs

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -85,6 +85,12 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+var suma = 0
+
+for(var i = 0; i < numeros.length; i++) {
+  suma = suma + numeros [i];
+  }
+return suma;
 }
 
 
@@ -92,6 +98,7 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  return agregarNumeros(resultadosTest) / resultadosTest.length;
 }
 
 
@@ -99,6 +106,13 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  var maximo = numeros[0];
+  for(var i = 1; i < numeros.length; i++) {
+    if(numeros[i] > maximo) { 
+      maximo = numeros[i];
+    }
+  }
+  return maximo;
 }
 
 
