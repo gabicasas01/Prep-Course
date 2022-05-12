de7a810f825849ca7164832523b91724646ceb78
gabicasas01 <casas.gf7@gmail.com>
2022-05-12 15:39:28 -0300

    prueba test

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -23,6 +23,7 @@ function crearUsuario() {
   return Usuario;
 }
 
+
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
